275d4c3dccdf8fb5ab2985097488eec3b6bdfcb5
Ariel Cuello <nive54@gmail.com>
2021-10-18 20:30:45 -0300

    clase2.js

---
+++ clase2.js
@@ -0,0 +1,52 @@
+//Mayor de edad
+const age = 20
+
+if (age >=18){
+    console.log("Es mayor de edad")
+}else {
+    console.log("Es menor de edad")
+}
+
+//Suma
+const a = 3
+const b = 2
+var suma = 0
+suma = a + b
+if(suma > a){
+    console.log(a)
+}
+if(suma > b){
+    console.log(b)
+}
+
+//C. Par/Impar
+const c = 14
+var parimpar = c % 2
+
+if(parimpar == 0){
+    console.log("Es par")
+}else {
+    console.log("Es impar")
+}
+
+const dia = 7
+switch(dia){
+    case 1: 
+        console.log("Lunes")
+        break
+    case 2:
+        console.log("Martes")
+        break
+    case 3: 
+        console.log("Miercoles")
+        break
+    case 4:
+        console.log("Jueves")
+        break
+    case 5: 
+        console.log("Viernes")
+        break
+    default:
+        console.log("Fin de semana")
+
+}
