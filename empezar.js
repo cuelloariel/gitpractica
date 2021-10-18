18a18254caa42656385582c1f42af7a6837bba18
Ariel Cuello <nive54@gmail.com>
2021-10-18 19:19:19 -0300

    empezar.js

---
+++ empezar.js
@@ -0,0 +1,14 @@
+
+const name = "Ariel" //crea variable para el nombre y le asigna un nombre
+const lastName = "Cuello" //crea variable para apellido y le asigna un apellido
+var age = 44 //crea variable para la edad y le asigna un numero
+
+//muestra en pantalla los datos Nombre, Apellido y Edad concatenados
+console.log (`Nombre: ${name}, Apellido:  ${lastName}, Edad: ${age}`)
+
+age = age + 1 //suma un año a la edad
+
+console.log(age) //muestra solo la edad con el año agregado
+
+//muestra en pantalla los datos completos con la nueva edad, concatenados
+console.log (`Nombre: ${name}, Apellido:  ${lastName}, Edad: ${age}`)
